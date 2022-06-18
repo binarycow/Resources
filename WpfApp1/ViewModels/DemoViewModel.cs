ffb55793f41aa54bad6b33d62488c4875ef41b62
Mike Christiansen <michael.a.christiansen10@gmail.com>
2022-06-18 18:02:48 -0400

    Add EnumIsCheckedExtension

---
+++ WpfApp1/ViewModels/DemoViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfApp1.ViewModels;
+
+public partial class DemoViewModel : ObservableObject
+{
+    [ObservableProperty] private Dock dock;
+}
