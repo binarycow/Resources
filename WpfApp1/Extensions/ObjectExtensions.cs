8843aa45190c7f87db7754564d8966f63302001e
Mike Christiansen <michael.a.christiansen10@gmail.com>
2022-07-31 13:20:00 -0400

    Extensions

---
+++ WpfApp1/Extensions/ObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WpfApp1;
+
+public static class ObjectExtensions
+{
+    public static bool Is<T>(this object? value, [MaybeNullWhen(false)] out T result)
+    {
+        result = default;
+        if (value is not T typed) 
+            return false;
+        result = typed;
+        return true;
+    }
+    
+    [return: NotNullIfNotNull("specifiedDefault")]
+    public static T? As<T>(this object? value, T? specifiedDefault = default) 
+        => value is T typed ? typed : specifiedDefault;
+}
